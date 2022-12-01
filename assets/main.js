983c7ad1686df626d43b3374e882bd59a9f5c3e6
Alissa Cohan <alissacohan18@gmail.com>
2022-11-30 20:05:56 -0500

    pseudo code

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+// pull local storage for search history
+// display where? drop down, button, other?
+    // create elements as needed to display
+        //TODO: id/class in html for display location
+
+// input field for user to select city
+    //TODO: need label & input in html
+    //TODO: need button w/ an id in html
+// function to:
+    // api fetch for weather
+    // display city selected
+        //TODO: id/class for location in html
+    // locally store city
+
+    // initially for music: user input or hard code weather key/value
+    // eventually: use key from weather api relating to temp and/or wind and/or rain/snow
+    // api fetch for song lists
+    // code to play song list x based on whether y
+    // function or loop that pulls weather key (resulting in playlist)
+    
+// event listener for submit button
