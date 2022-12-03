51182a6f6d24f864193d6c7fb90be16e40accd1b
Daikeen Lockett <daikeen_lockett@yahoo.com>
2022-12-03 00:18:13 -0500

    Added embed for hot weather

--- assets/main.js
+++ assets/main.js
@@ -74,8 +74,10 @@ let weather = {
 
     if(temp > 75 ) {
       console.log(`it's hot`)
+      $("#embed").html(`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX1BzILRveYHb?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`)
     } else if(temp <= 75 && temp >= 60) {
-      console.log(`it's warm`)    
+      console.log(`it's warm`)  
+        
     } else {
       console.log(`it's cold`)
     }
