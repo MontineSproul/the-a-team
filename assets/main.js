998122b19a907d02a98b98bb73b44c364e979029
Daikeen Lockett <daikeen_lockett@yahoo.com>
2022-12-03 00:19:57 -0500

    Added embed for warm weather

--- assets/main.js
+++ assets/main.js
@@ -77,7 +77,7 @@ let weather = {
       $("#embed").html(`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX1BzILRveYHb?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`)
     } else if(temp <= 75 && temp >= 60) {
       console.log(`it's warm`)  
-        
+      $("#embed").html(`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX5IDTimEWoTd?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`)  
     } else {
       console.log(`it's cold`)
     }
