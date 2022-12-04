96d9b1926774ec41b3c8e7e5e5224a158d5e261c
Daikeen Lockett <daikeen_lockett@yahoo.com>
2022-12-03 19:03:11 -0500

    created conditions to apply 'hot','warm',or'cold' classes to 'main' element

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,21 +66,22 @@ let weather = {
     spotifyEmbed.attr("id", "embed");
     spotifyEl.append(spotifyEmbed);
 
-    // Hot weather is temp > 75
-    // Warm weather is when temp is < 75 and > 60
-    // Cold weather is < 60
+    // Hot weather is temp > 75; Warm weather is when temp is < 75 and > 60; Cold weather is < 60
     if (temp > 75) {
       console.log(`it's hot`);
+      mainEl.attr("class", "hot")
       $("#embed")
         .html(`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX1BzILRveYHb?utm_source=generator" 
       width="100%" height="380" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`);
     } else if (temp <= 75 && temp >= 60) {
       console.log(`it's warm`);
+      mainEl.attr("class", "warm")
       $("#embed")
         .html(`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX5IDTimEWoTd?utm_source=generator" 
       width="100%" height="380" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`);
     } else {
       console.log(`it's cold`);
+      mainEl.attr("class", "cold")
       $("#embed")
         .html(`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX97m5YXQMpCi?utm_source=generator" 
       width="100%" height="380" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`);
